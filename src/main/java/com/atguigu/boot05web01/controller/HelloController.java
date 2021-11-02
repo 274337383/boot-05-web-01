58c667bbf1b09b427dac0821b53a8e0b76837fd3
27433 <274337383@com>
2021-11-02 21:02:29 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/boot05web01/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.atguigu.boot05web01.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.HTML;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String getUser() {
+        return "GET-张三";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String saveUser() {
+        return "POST-张三";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public String putUser() {
+        return "PUT-张三";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String deleteUser() {
+        return "DELETE-张三";
+    }
+}
